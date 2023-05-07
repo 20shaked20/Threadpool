c690d2bce74a8c1e2e375ed8511d07d7d56bd092
Shaked Levi <73894107+20shaked20@users.noreply.github.com>
2023-05-07 16:26:06 +0300

    Update our_codec.cpp
    Added support to multiple line file + some functionality changed

--- our_codec.cpp
+++ our_codec.cpp
@@ -6,31 +6,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <sys/sysinfo.h> // used in getting the cores//
 #include <pthread.h>
 #include <cstring>
 #include <fstream>
 #include <sstream>
-#include <iostream> 
+#include <iostream>
 #include <queue> // used in task and thread pools
 #include <dlfcn.h>
 
-void (*encrypt_func)(char *s, int key);
-void (*decrypt_func)(char *s, int key);
+void (*encrypt_data)(char *s, int key);
+void (*decrypt_data)(char *s, int key);
 void *handle_task(void *arg);
 void *handle_threads(void *arg);
-
 
 using namespace std;
 
 typedef struct Task
 {
 
-    char tape[1024];   // the string the task holds.
-    bool finished; // is the task finished doing the ecnrpyt/decrpyt
-    char type[1024];     // encode -> '-e' | decode -> '-d' //
-    int index;     // n.o of the task in the pool
+    char tape[1024]; // the string the task holds.
+    bool finished;   // is the task finished doing the ecnrpyt/decrpyt
+    char type[2];    // encode -> '-e' | decode -> '-d' //
+    int index;       // n.o of the task in the pool
     int key;
 
 } Task;
@@ -60,12 +59,37 @@ void init_thread_pool()
 
     for (int i = 0; i < cores; ++i)
     {
-
         pthread_t new_thread;
         t_pool.push(new_thread);
     }
 }
 
+/**
+ * This is taken from stackoverflow:
+ * https://stackoverflow.com/questions/17081131/how-can-a-shared-library-so-call-a-function-that-is-implemented-in-its-loader
+ * i didnt know how to call the functions by its own, because it didnt work.
+ * so google helped.
+ */
+int call_library_functions()
+{
+    void *handle = NULL;
+    handle = dlopen("./libCodec.so", RTLD_NOW | RTLD_GLOBAL);
+    if (handle == NULL)
+    {
+        fprintf(stderr, "Unable to open lib: %s\n", dlerror());
+        return -1;
+    }
+    encrypt_data = (void (*)(char *s, int key))dlsym(handle, "encrypt");
+    decrypt_data = (void (*)(char *s, int key))dlsym(handle, "encrypt");
+
+    if (encrypt_data == NULL || decrypt_data == NULL)
+    {
+        fprintf(stderr, "Unable to get function\n");
+        return -1;
+    }
+
+    return 0;
+}
 
 /**
  * Main thread handler, is used by admin_thread which assings the threads on diffrent tasks.
@@ -102,18 +126,49 @@ void *handle_threads(void *arg)
     }
 }
 
+/**
+ * This method handles the task and simply encrpyt/decrpyts and prints the data.
+ */
+void *handle_task(void *arg)
+{
+    struct Task *active_task = (struct Task *)arg; // import the given task to a Task type
+
+    char *charArray = &active_task->tape[0];
+    if (active_task->type[1] == 'e')
+    {
+        encrypt_data(charArray, active_task->key);
+    }
+    else if (active_task->type[1] == 'd')
+    {
+        decrypt_data(charArray, active_task->key);
+    }
+
+    pthread_mutex_lock(&lockPrint);
+
+    while (task_number < active_task->index) // this loop is used in order to make sure we print accordinly, and not in some messed up order.
+    {
+        pthread_cond_wait(&WaitCond, &lockPrint);
+    }
+
+    cout << active_task->tape << endl; // prints the data on tape.
+    fflush(stdout);                                // clears stdout, is important to make sure the buffer is cleared, so we dont get some junk.
+
+    task_number++; // increse the task number, is important for the loop above.
+    pthread_cond_broadcast(&WaitCond);
+
+    t_pool.push(pthread_self()); // thread is done, and we push it to the pool again so it can pick up new tasks.
+
+    pthread_cond_broadcast(&TaskCond); // tells that a task is ongoing
+    pthread_mutex_unlock(&lockPrint);
+
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    
-    /* code */
-    void* handle = dlopen("./libCodec.so", RTLD_LAZY | RTLD_GLOBAL);
-    if(!handle){
-        fprintf(stderr, "dlerror: %s\n", dlerror());
-        exit(EXIT_FAILURE);
-    }
-    encrypt_func = (void (*)(char *s, int key))dlsym(handle,"encrypt");
-    decrypt_func = (void (*)(char *s, int key))dlsym(handle,"decrypt");
-    
+
+    call_library_functions();
+
     init_thread_pool();
 
     pthread_create(&admin_thread, NULL, handle_threads, NULL); // creates the thread that handles all the threads.
@@ -123,30 +178,25 @@ int main(int argc, char const *argv[])
 
         // reads the data//
         std::string data;
-        std::ifstream file(argv[4]); // open the input file
-        if (file.peek() != EOF)
-        {                             // case where its a file //
-            std::stringstream buffer; // string stream
-            buffer << file.rdbuf();   // read file to the string stream
-            data = buffer.str();      // imports the buffer to a normal string.
-        }
-        else // case when we enter a string manually //
-        {
-            std::getline(std::cin, data);
+
+        for (std::string line; std::getline(std::cin, line);) {
+            data+=line;
         }
 
         // split the data into tasks of 1024 bytes//
         int data_size = data.size();
+        // printf("DATA SIZE : %d \n", data_size);
         int pos = 0; // 0 -> 1024 , 1024 -> 2048
 
         while ((data_size - 1024) > 0 && pos + 1024 < data.size())
         {
 
             /*building new task to add*/
-            struct Task *new_task = (struct Task*)malloc(sizeof(struct Task));
+            struct Task *new_task = (struct Task *)malloc(sizeof(struct Task));
             char dataArray[1024];
             memset(dataArray, '\0', 1024);
             std::strncpy(dataArray, data.substr(pos, 1024).c_str(), 1024);
+            printf("%s", dataArray);
             memset(new_task->tape, '\0', 1024);
             strcpy(new_task->tape, dataArray);
             strcpy(new_task->type, argv[2]);
@@ -155,7 +205,6 @@ int main(int argc, char const *argv[])
             new_task->index = active_tasks;
             task_pool.push(*new_task);
 
-            
             pos += 1024;
             data_size -= 1024;
 
@@ -166,9 +215,9 @@ int main(int argc, char const *argv[])
         // edge case for when we get scenario that data is not splitted by 1024 //
         if (data_size > 0)
         {
-            
+
             /*building new task to add*/
-            struct Task *new_task = (struct Task*)malloc(sizeof(struct Task));
+            struct Task *new_task = (struct Task *)malloc(sizeof(struct Task));
             char dataArray2[1024];
             memset(dataArray2, '\0', 1024);
             std::strncpy(dataArray2, data.substr(pos, data_size).c_str(), data_size);
@@ -181,48 +230,14 @@ int main(int argc, char const *argv[])
             task_pool.push(*new_task);
             ++active_tasks;
             sum_of_tasks = active_tasks;
-           
-            
+
             pthread_cond_broadcast(&TaskCond); // when done with tasks, init the threads.
         }
     }
-    while(true){
-       ;
-    }
-return 0;
-}
-
-/**
- * This method handles the task and simply encrpyt/decrpyts and prints the data.
- */
-void *handle_task(void *arg)
-{
-    struct Task *active_task = (struct Task *)arg; // import the given task to a Task type
-    
-    char* charArray = &active_task->tape[0];
-    if (active_task->type[1] == 'e') {
-        encrypt_func(charArray, active_task->key);
-    }
-    else if (active_task->type[1] == 'd') {
-        decrypt_func(charArray, active_task->key);
-    }
-
-    pthread_mutex_lock(&lockPrint);
-
-    while (task_number < active_task->index) // this loop is used in order to make sure we print accordinly, and not in some messed up order.
+    while (true)
     {
-        pthread_cond_wait(&WaitCond, &lockPrint);
+        if (sum_of_tasks == task_number)
+            break;
     }
-
-    cout << "DATA: " << active_task->tape << endl; // prints the data on tape.
-    fflush(stdout); //clears stdout, is important to make sure the buffer is cleared, so we dont get some junk.
-
-    task_number++; //increse the task number, is important for the loop above.
-    pthread_cond_broadcast(&WaitCond);
-
-    t_pool.push(pthread_self()); //thread is done, and we push it to the pool again so it can pick up new tasks.
-
-    pthread_cond_broadcast(&TaskCond); //tells that a task is ongoing
-    pthread_mutex_unlock(&lockPrint);
+    return 0;
 }
-
